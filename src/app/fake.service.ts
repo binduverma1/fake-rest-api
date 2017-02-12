3b925603fa6c88d426ee2376ed0b6cbe44a7fa3a
bhuvanarajamani <bhuvana.cr@gmail.com>
2017-02-12 01:28:33 -0600

    Data path

--- src/app/fake.service.ts
+++ src/app/fake.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw'
 
-const BOOKS_API: string = "/books";
+const BOOKS_API: string = "./assets/data.json";
 
 @Injectable()
 export class FakeService {
@@ -17,7 +17,7 @@ export class FakeService {
   getBooks(): Observable<any[]>{
     return this.http
       .get(BOOKS_API)
-      .map(resp => resp.json())
+      .map(resp => resp.json().books)
       .catch((error: any) => Observable.throw(error.json()));
   }
 
